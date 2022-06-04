200e135c0ddfaf131fe0062f9c52c1394a901553
sypniewski <m.odrowaz.sypniewski@gmail.com>
2022-06-04 17:38:45 +0200

    wip

--- ml/create_images.py
+++ ml/create_images.py
@@ -7,7 +7,7 @@ import cv2
 
 def create_db():
     if not os.path.exists("images"):
-        os.makedirs("images");
+        os.makedirs("images")
     if not os.path.exists("migai_db.db"):
         conn = sqlite3.connect("migai_db.db")
         conn.execute("CREATE TABLE migai (id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE)")
@@ -19,7 +19,6 @@ def add_gesture(name):
         conn.execute("INSERT INTO migai (name) VALUES (?)", (name,))
     except sqlite3.IntegrityError:
         print("Gesture already in database")
-        print(conn.execute("SELECT * FROM migai").fetchall())
         return conn.execute("SELECT id FROM migai WHERE name == (?)", (name,)).fetchall()[0][0]
     conn.commit()
     return conn.execute("SELECT id FROM migai WHERE name == (?)", (name,)).fetchall()[0][0]
@@ -39,7 +38,6 @@ def take_images(init_pic_no, id):
         if started:
             cv2.imwrite("images/"+str(id)+"/"+str(pic_no)+".jpg", gray)
             pic_no += 1
-            print(pic_no)
         cv2.imshow("image", gray)
         key = cv2.waitKey(1)
         if key == ord('x'):
@@ -47,15 +45,16 @@ def take_images(init_pic_no, id):
                 started = False
                 total = init_pic_no
             else:
-                print("started")
+                print("Started capturing")
                 started = True
         if pic_no == total:
             return
 
 create_db()
-name = input("Image name:...")
+name = input("Gesture name:...")
 init_pic_no = int(input("Initial picture number:..."))
 id = add_gesture(name)
 if not os.path.exists("images/"+str(id)):
     os.makedirs("images/"+str(id));
+print("`x` to start taking images")
 take_images(init_pic_no, id)
