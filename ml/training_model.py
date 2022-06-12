d1bdc56126750e49294dc89ebf910a2012a77061
sypniewski <m.odrowaz.sypniewski@gmail.com>
2022-06-12 15:04:45 +0200

    wip

--- ml/training_model.py
+++ ml/training_model.py
@@ -1,29 +1,71 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import datasets
+from torchvision.transforms import ToTensor
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Meta-parameters
-epochs = 10
-batch_size = 4
+epochs = 4
+batch_size = 16
 learning_rate = 0.001
 
+# Loading data
+train_data = datasets.ImageFolder("train/", transform=ToTensor())
+test_data = datasets.ImageFolder("test/", transform=ToTensor())
+
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+testloader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True)
+
+# train_data = datasets.MNIST(
+#     root = 'data',
+#     train = True,
+#     transform = ToTensor(),
+#     download = True,
+# )
+# test_data = datasets.MNIST(
+#     root = 'data',
+#     train = False,
+#     transform = ToTensor()
+# )
+#
+# trainloader = torch.utils.data.DataLoader(train_data, batch_size=100, shuffle=True)
+# testloader = torch.utils.data.DataLoader(test_data, batch_size=100, shuffle=True)
+
+
+print(train_data)
+print(test_data)
+print(train_data.data.size())
+print(train_data.targets.size())
+
+figure = plt.figure(figsize=(10, 8))
+cols, rows = 5, 5
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(train_data), size=(1,)).item()
+    img, label = train_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(label)
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
 
 class ConvNet(nn.Module):
     def __init__(self):
-        self.conv1 = nn.Conv2d(1, 6, 3)
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 6, 3)
-        self.conv3 = nn.Conv2d(6, 16, 3)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+#        self.conv3 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16*5*5, 120)
-        self.fc2 = nn.Linear(120, 48)
-        self.fc3 = nn.Linear(48, 20)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 22)
 
     def forward(self, x):
         # Convolutional and pooling layers
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        #x = self.pool(F.relu(self.conv3(x)))
         # Flattening
         x = torch.flatten(x, 1)
         # Fully connected layers
@@ -33,5 +75,39 @@ class ConvNet(nn.Module):
         return x
 
 
-net = ConvNet()
-print(net)
+model = ConvNet()
+print(model)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+
+for epoch in range(epochs):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if (i+1) % 50 == 0:
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 50:.3f}')
+            running_loss = 0.0
+
+print("Finished Training")
+
+torch.save(model.state_dict(), './model.pth')
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
